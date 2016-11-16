39a99678e1803b345f23b48d5afaf674a3c93401
YenJung <SmallLie1203@gmail.com>
2016-11-16 08:05:52 -0500

    Site updated: 2016-11-16 08:05:50

---
+++ uncategorized/Line-echobot-php/code/callback.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('./EchoBot.php');
+
+$channelAccessToken = 'your token';
+$channelSecret = 'your channel secret';
+
+$client = new EchoBot($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
